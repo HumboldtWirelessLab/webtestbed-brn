7dee7de3a40073c7ef4d14889519579b550f9c38
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2009-10-13 00:35:17 +0200

    update

--- host/worker/run_measurement.sh
+++ host/worker/run_measurement.sh
@@ -2,7 +2,11 @@
 
 . $2
 
-(cd $1; $HELPER_HOME/simulation/bin/ns2-sim.sh run ./testbed_$1.dis 1)
+if [ "x$MODE" == "xsim" ]; then
+  (cd $1; $HELPER_HOME/simulation/bin/ns2-sim.sh run ./testbed_$1.dis 1)
+else
+  (cd $1; $HELPER_HOME/measurement/bin/run_measurement.sh ./testbed_$1.dis 1)
+fi
 (cd $1; rm -rf result; rm -rf result.tar.bz2)
 (cd $1; mv ./1 ./result; tar cvfj ./result.tar.bz2 ./result > /dev/null)
 
